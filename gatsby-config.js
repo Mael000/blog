50beafd54a339f4b2ab0dce29e5a2a7496529979
Davide Bellone <davide.bellone>
2020-02-18 14:25:52 +0100

    Try update google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -126,7 +126,7 @@ module.exports = {
         id: config.Google_Tag_Manager_ID,
         // Include GTM in development.
         // Defaults to false meaning GTM will only be loaded in production.
-        includeInDevelopment: false,
+        includeInDevelopment: true,
       },
     },
     {
@@ -169,6 +169,24 @@ module.exports = {
       resolve: 'gatsby-plugin-draft',
       options: {
         publishDraft: process.env.NODE_ENV !== 'production',
+      },
+    },
+
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          config.Google_Tag_Manager_ID, // Google Analytics / GA
+             ],
+ 
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true 
+        },
       },
     },
 
