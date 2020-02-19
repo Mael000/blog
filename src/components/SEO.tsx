ebdc222b457e4144f68f923e3af089439896e6c7
Davide Bellone <davide.bellone>
2020-02-19 11:22:28 +0100

    WIP google analytics on routing? #29

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -30,7 +30,7 @@ export const SEO = (props: SEO) => {
     console.table(postNode.frontmatter);
 
     description = postNode.frontmatter.description || postNode.excerpt;
-    image = postNode.frontmatter?.banner?.replace('{format}', 'c_scale,w_520') ?? config.defaultArticleBanner;
+    image = postNode.frontmatter?.banner?.replace('{format}', '') ?? config.defaultArticleBanner;
     postURL = config.siteUrl + realPrefix + postPath;
   } else {
     title = pageTitle || config.siteTitle;
